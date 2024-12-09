995aa25e862011a82d0e151c57819b75b8577029
romaluck <romeo91222@gmail.com>
2024-12-09 22:01:51 +0100

    fix: move logout sittings to security.yaml

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,12 +9,6 @@ use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
-
     #[Route('/api/csrf-token', name: 'review_csrf_token', methods: ['GET'])]
     public function getCsrfToken(CsrfTokenManagerInterface $csrfTokenManager): Response
     {
